a5864e1e86301f26d814a6ca9946b6d180b4e552
Francisco Losada <99215889+LosadaT@users.noreply.github.com>
2025-04-19 17:36:44 -0300

    Add files via upload

---
+++ OnboardFlow/OnboardFlow/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  Content2View.swift
+//  Aula
+//
+//  Created by Francisco Totaro on 18/04/25.
+//
+
+import SwiftUI
+
+var gradientColors: [Color] = [
+    .gradientTop,
+    .gradientBottom
+]
+
+struct ContentView: View {
+    var body: some View {
+        TabView{
+            WelcomePage()
+            FeaturesPage()
+        }
+        .background(Gradient(colors: gradientColors))
+        .tabViewStyle(.page)
+        .foregroundStyle(.white)
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OnboardFlow/OnboardFlow/OnboardFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardFlowApp.swift
+//  OnboardFlow
+//
+//  Created by Francisco Totaro on 19/04/25.
+//
+
+import SwiftUI
+
+@main
+struct OnboardFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
