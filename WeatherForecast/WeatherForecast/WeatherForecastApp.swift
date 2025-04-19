fd67660a81b62b6423634696932532e643cb0368
Francisco Losada <99215889+LosadaT@users.noreply.github.com>
2025-04-19 17:36:25 -0300

    Add files via upload

---
+++ WeatherForecast/WeatherForecast/WeatherForecastApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherForecastApp.swift
+//  WeatherForecast
+//
+//  Created by Patricia Totaro on 19/04/25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherForecastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
