0e9bc652511ef397caa14809bfa074c920a1500e
Francisco Losada <99215889+LosadaT@users.noreply.github.com>
2025-04-19 17:24:11 -0300

    Add files via upload
    Aula3

---
+++ DiceRoller/DiceRoller/DiceRollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollerApp.swift
+//  DiceRoller
+//
+//  Created by Patricia Totaro on 19/04/25.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
