d3c33e439f63071554906ea267dca81f9cfd6512
Francisco Losada <99215889+LosadaT@users.noreply.github.com>
2025-04-20 12:17:56 -0300

    Add files via upload

---
+++ ScoreKeeper/ScoreKeeper/Player.swift
@@ -0,0 +1,16 @@
+//
+//  Player.swift
+//  ScoreKeeper
+//
+//  Created by Patricia Totaro on 20/04/25.
+//
+
+import Foundation
+
+struct Player: Identifiable{
+    let id = UUID()
+    var name: String
+    var score: Int
+}
+
+
---
+++ ScoreKeeper/ScoreKeeper/ScoreKeeperApp.swift
@@ -0,0 +1,18 @@
+//
+//  ScoreKeeperApp.swift
+//  ScoreKeeper
+//
+//  Created by Patricia Totaro on 20/04/25.
+//
+
+import SwiftUI
+
+@main
+struct ScoreKeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
